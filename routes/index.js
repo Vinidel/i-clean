8f8589d13d852414fa4cdd0194470967f92b5ad9
Vinicius <vinidroid@gmail.com>
2016-07-06 19:26:12 +1000

    Creating the real dashboard

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,8 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/dashboard', function(req, res, next) {
+    res.render('main/index');
+});
+
 module.exports = router;
