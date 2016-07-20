ae43db1d6c891f641fbc94f63d2f3be76658c8d2
Vinicius <vinidroid@gmail.com>
2016-07-20 19:24:10 +1000

    VINIDEL_REGISTER_20072016: Front end register

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,36 @@
 /**
  * Created by vinidev on 20/07/16.
  */
+(function(){
+
+function routerConfig($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/landing');
+
+    $stateProvider
+        .state('landing', {
+            url: '/landing',
+            views: {
+                'landingPage': {
+                    templateUrl: '/landing.html', controller: 'LandingController', controllerAs: 'landingCtrl'
+                }
+            }
+        })
+}
+
+function LandingController(){
+    var self    = this;
+    self.test   = test;
+
+    test();
+
+    function test(){
+        console.log('Heyyy yaaaaa');
+    }
+}
+
+
+angular.module('LandingPageModule',['ui.router', 'ui.bootstrap'])
+    .controller('LandingController', LandingController)
+    .config(routerConfig)
+})();
+
