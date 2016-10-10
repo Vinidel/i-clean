1772ce558ff9bc77ef04fb4d5d11f0e140e62528
Vinicius Delascio Fortunato <vinny@192-168-1-102.tpgi.com.au>
2016-10-10 21:51:58 +1100

    feat-13: Change app.js to ES6

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
-var express         = require('express');
-var path            = require('path');
-var favicon         = require('serve-favicon');
-var logger          = require('morgan');
-var cookieParser    = require('cookie-parser');
-var bodyParser      = require('body-parser');
-var consign         = require('consign');
-var load            = require('express-load');
-var mongoose        = require('mongoose');
-var mongoConf       = require('./config/mongo');
-var passport        = require('passport');
-var flash           = require('connect-flash');
-var session         = require('express-session');
-
-var app             = express();
-
-//var routes          = require('./routes/index');
-//var users           = require('./routes/users');
-
-
+const express         = require('express');
+const path            = require('path');
+const favicon         = require('serve-favicon');
+const logger          = require('morgan');
+const cookieParser    = require('cookie-parser');
+const bodyParser      = require('body-parser');
+const load            = require('express-load');
+const mongoose        = require('mongoose');
+const mongoConf       = require('./config/mongo');
+const passport        = require('passport');
+const flash           = require('connect-flash');
+const session         = require('express-session');
+const app             = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 
 
 // uncomment after placing your favicon in /public
@@ -32,13 +24,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-//
-//consign()
-//    .include('models')
-//    .then('controllers')
-//    .into(app);
-
 
 
 // required for passport
@@ -64,7 +49,7 @@ mongoose.connection.on('error', function(err) {
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
